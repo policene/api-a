0badea1b40b89330e0961144e24d00820c2aa485
GIOVANNI CORADASSI POLICENE <giovanni.policene@cs.up.edu.br>
2025-04-16 21:23:24 -0300

    feat: redis

--- app.js
+++ app.js
@@ -1,45 +1,74 @@
 const express = require('express');
 const axios = require('axios');
+const redis = require('redis');
 const app = express();
 const port = 4000;
 
-// Rota que consome os dados da primeira API
+let client;
+
+const getRecommendation = (temperature) => {
+    if (temperature > 30) {
+        return "Hidratação!";
+    }
+    if (temperature >= 16 && temperature <= 29) {
+        return "O clima está agradável";
+    }
+    if (temperature < 15) {
+        return "Usa casaco";
+    }
+};
+
 app.get('/recommendation', async (req, res) => {
-    const cityName = req.query.city;
+    try {
+        if (!client) {
+            client = redis.createClient({
+                host: 'localhost',
+                port: 6379
+            });
 
-    if (!cityName) {
-        return res.status(400).send("Please, specify the city's name.")
-    }
+            client.on('error', (err) => {
+                console.log('Erro no Redis: ', err);
+            });
 
-    const getRecommendation = (temperature) => {
-        if (temperature > 30) {
-            return "Hidratação!";
-        } 
-        if (temperature >= 16 && temperature <= 29) {
-            return "O clima está agradável";
+            await client.connect();
         }
-        if (temperature < 15) {
-            return "Usa casaco";
+
+        const cityName = req.query.city;
+
+        if (!cityName) {
+            return res.status(400).send("Por favor, especifique o nome da cidade.");
         }
-    }
 
-    try { 
-        const response = await axios.get(`http://localhost:3000/weather?city=${encodeURIComponent(cityName)}`)
+        const cacheKey = cityName;
 
-        const temperature = response.data.temp;
+        let data = await client.get(cacheKey);
+        data = data ? JSON.parse(data) : null;
 
-        const recommendationString = getRecommendation(temperature)
+        if (!data) {
+            try {
+                const response = await axios.get(`http://localhost:3001/weather/${encodeURIComponent(cityName)}`);
+
+                await client.setEx(cacheKey, 60, JSON.stringify(response.data));
+
+                data = response.data;
+            } catch (error) {
+                return res.status(404).send("Cidade não encontrada na API de meteorologia.");
+            }
+        }
+
+        const temperature = data.temp;
+        const recommendationString = getRecommendation(temperature);
 
         return res.json({
-            temperatura: response.data,
+            temperatura: data,
             recommendation: recommendationString
         });
-    } catch (error) {
-        return res.status(404).send("Cidade não encontrada na API de meteorologia.");
-    }
 
+    } catch (error) {
+        return res.status(500).send("Erro interno no servidor.");
+    }
 });
 
 app.listen(port, () => {
-  console.log(`API A rodando em http://localhost:${port}`);
+    console.log(`API rodando em http://localhost:${port}`);
 });
